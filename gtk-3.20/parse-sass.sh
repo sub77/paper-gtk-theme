fef3d773ca5eade33b66691e12c1092274e2f979
sub77 <sub77@ymail.com>
2017-04-09 19:26:11 +0200

    modify parsing script

--- gtk-3.20/parse-sass.sh
+++ gtk-3.20/parse-sass.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 
+if [ -f gtk*.css ]; then rm gtk*.css; echo "rm *.css"; else "not found"; fi
+
 #bundle exec sass --update --sourcemap=none .
 scss --update --sourcemap=none .
+
+gsettings set org.mate.interface gtk-theme PaperUser; sleep 5
+gsettings set org.mate.interface gtk-theme PaperMod
