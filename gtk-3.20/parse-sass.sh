2323ca5464f2e6412d0d9272e525e6f9c8dd1059
sub77 <sub77@ymail.com>
2017-04-11 22:55:00 +0200

    window picker right alignment

--- gtk-3.20/parse-sass.sh
+++ gtk-3.20/parse-sass.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-if [ -f gtk*.css ]; then rm gtk*.css; echo "rm *.css"; else "not found"; fi
-
+if [[ -f gtk.css ]]; then rm gtk.css; echo "rm gtk.css"; else echo "gtk.css not found"; fi
+if [[ -f gtk-dark.css ]]; then rm gtk-dark.css; echo "rm gtk-dark.css"; else echo "gtk-dark.css not found"; fi
 #bundle exec sass --update --sourcemap=none .
 scss --update --sourcemap=none .
 
